7103f308e0bdd40df5d509feba1ac20d9dfef599
naveenseedani21 <ns75004@uga.edu>
2025-02-08 04:20:48 -0500

    finished the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import SignupPage from './pages/SignupPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
 import "./App.css"
 
 function Home() {
@@ -11,6 +12,11 @@ function Home() {
       <button onClick={() => navigate('/signup')}>Go to Signup</button>
     </div>
   );
+  return (
+    <div>
+      <button onClick={() => navigate('/login')}>Go to Signup</button>
+  </div> 
+  );
 }
 
 function App() {
@@ -19,6 +25,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
