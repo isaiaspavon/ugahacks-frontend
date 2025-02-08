b1b2f7f18c4ed621151db2ade274eaa78d128ca4
JoshuaCS01 <jtc55494@uga.edu>
2025-02-08 03:05:40 -0500

    Login Page worked on

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+ <div class="container">
+    <div class="card">
+      <h1 class="header-text">Create Your Account</h1>
+      <p class="subtext">Join us to get started!</p>
+
+      <div class="auth-buttons">
+        <button class="auth-button">Sign up with Google</button>
+        <button class="auth-button">Sign up with Apple</button>
+      </div>  
+        <div class="hr-container">
+          <hr/>
+          <span class="or-text">or</span>
+          <hr/>
+          </div>
+
+      <div class="email-container">
+        <label for="email" class="email-label">Email Address</label>
+        <input type="email" id="email" class="email-input" placeholder="Enter your email"/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <button class="continue-button">Continue</button>
+
+      <div class="no-account">
+        <span class="no-account-text">Already have an account?</span>
+        <span class="create-one">Log in</span>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  </div>
+
   )
 }
 
