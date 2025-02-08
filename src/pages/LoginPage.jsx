fdca78e35681d9f224692073522e464b73c23342
naveenseedani21 <ns75004@uga.edu>
2025-02-08 09:52:57 -0500

    clicking logo takes to home now

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,16 +4,16 @@ import styles from './LoginPage.module.css'; // Import CSS module
 import logo from '../assets/LOGO for WibeCheck.jpg';
 
 function LoginPage() {
-  const [count, setCount] = useState(0);
   const navigate = useNavigate();
 
   return (
     <div className={styles.container}>
       <div className={styles.card}>
-        <img className={styles.logo} src={logo} alt="LOGO"></img>
+        <button className={styles.logo} onClick={() => navigate('/')}>
+            <img className={styles.logo} src={logo} alt="LOGO" height="80" width="80" ></img>
+        </button>
         <h1 className={styles.headerText}>Sign In to WibeCheck</h1>
         <p className={styles.subtext}>Welcome back! Please sign in to continue.</p>
-
         <div className={styles.authButtons}>
           <button className={styles.authButton}>Google</button>
         </div>  
@@ -30,11 +30,17 @@ function LoginPage() {
           <input type="email" id="email" className={styles.emailInput} placeholder="Enter your email address" />
         </div>
 
+        {/* Password Input */}
+        <div className={styles.emailContainer}>
+          <label htmlFor="password" className={styles.emailLabel}>Password</label>
+          <input type="password" id="password" className={styles.emailInput} placeholder="Enter your password" />
+        </div>
+
         <button className={styles.continueButton}>Continue</button>
 
         <div className={styles.noAccount}>
           <span className={styles.noAccountText}>No Account?</span>
-          <span className={styles.createOne} onClick={() => window.location.href = '/signup'}>Create One</span>
+          <span className={styles.createOne} onClick={() => navigate('/signup')}>Create One</span>
         </div>
       </div>
     </div>
