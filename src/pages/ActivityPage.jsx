ae4b4527cb0a67b47d62edbe21a00a160998d44d
naveenseedani21 <ns75004@uga.edu>
2025-02-08 11:10:43 -0500

    Activity Page is Almost Done

--- src/pages/ActivityPage.jsx
+++ src/pages/ActivityPage.jsx
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBarActivity from '../components/NavBarActivity';
 import styles from './ActivityPage.module.css';
+import { useNavigate } from 'react-router-dom';
+import defaultProfile from '../assets/profileLogo.jpg';
 
 const ActivityPage = () => {
+    const [showImagePart, setShowImagePart] = useState(false);
+    const navigate = useNavigate();
+
     return (
-        <div className={styles.navBar}>
-            {/* Navbar */}
-            <NavBarActivity />
+        <>
+            <div className={styles.navBar}>
+                <NavBarActivity />
+            </div>
 
             {/* Main Content */}
-            <div className="flex-grow p-6">
-                <h1 className="text-3xl font-bold text-center">Welcome to the Activity Page</h1>
-                <p className="text-gray-700 text-center mt-4">
-                    This is where your activity content will go.
-                </p>
+            <div className={styles.page}>
+                <div className={styles.questionBox}>
+                    <h2 className={styles.gradientText}>Whats the Wibe?</h2>
+                    <button 
+                        className={styles.wibeButton} 
+                        onClick={() => setShowImagePart(true)}
+                    >
+                        Upload the Wibe
+                    </button>
+                </div>
+
+
+                {/* Image Part (Hidden Until Button is Clicked) */}
+                {showImagePart && (
+                                    <div invisiblePortion>
+                    <div className={`${styles.imagePart} ${showImagePart ? styles.show : ''}`}>
+                        <img src={defaultProfile}  width="200" height="200"></img>
+                        <p className={styles.imageWords}>Drag and drop an image or select one from storage.</p>
+                    </div>
+
+                    <button className={styles.homeButton} onClick={() => navigate("/")}>Analyze</button>
+                    </div>
+                
+                    
+                )}
             </div>
-        </div>
+        </>
     );
 };
 
