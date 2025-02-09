31312f23ff3b6925a72e20c875f410526619d06f
Isaias <isaiaspavon@Isaiass-MacBook-Pro.local>
2025-02-09 00:08:54 -0500

    Slowly working on NextAuth

---
+++ src/auth.ts
@@ -0,0 +1,75 @@
+import {authConfig} from "./auth.config";
+import NextAuth from "next-auth";
+
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import User from "./models/UserSchema";
+import connectMongoDB from "./libs/mongodb";
+
+export const{
+    handlers: {GET, POST},
+    auth,
+    signIn,
+    signOut
+} = NextAuth({
+    ...authConfig,
+    providers: [
+        CredentialsProvider({
+            credentials:{
+                email: {},
+                password: {},
+            },
+
+            async authorize(credentials) {
+              
+                if (!credentials) return null;
+                try {
+                  console.log("Attempting to find user...");
+                    await connectMongoDB();
+              
+                  const user = await User.findOne({ email: credentials.email }).lean();
+              
+                  if (user) {              
+                    const isMatch = await bcrypt.compare(credentials.password as string, user.password as string);
+              
+                    if (isMatch) {
+                      return {
+                        id: user._id.toString(),
+                        email: user.email,
+                        name: user.fName,
+
+                      };
+
+                    } else {
+                      console.log("Email or Password is not correct");
+                      return null;
+                    }
+                  } else {
+                    console.log("User not found");
+                    return null;
+                  }
+                } catch (error: any) {
+                  return null;
+                }
+              },
+        }),
+    ],
+
+    session: {
+      strategy: "jwt",
+    },
+    callbacks: {
+      async jwt({ token, user }) {
+        // Add user data to the token when available
+        if (user) {
+          token.id = user.id;
+        }
+        return token;
+      },
+      async session({ session, token }) {
+        // Attach token data to the session
+        session.user.id = token.id as string;
+        return session;
+      },
+    },
+});
